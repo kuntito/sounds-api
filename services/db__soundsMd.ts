53ff718613341010c0a4ec08dab221317de6b371
kuntito <owolabitito@gmail.com>
2025-12-26 19:19:40 +0000

    update db path for clarity

--- services/db__soundsMd.ts
+++ services/db__soundsMd.ts
@@ -1,6 +1,12 @@
 import Database from "better-sqlite3";
+import path from "path";
 
-const db__sounds_md = new Database("songs_md.db");
+const db_name = "songs_md.db"
+const db_path = path.join(
+    process.cwd(),
+    db_name,
+)
+const db__sounds_md = new Database(db_path);
 
 db__sounds_md.exec(`
     CREATE TABLE IF NOT EXISTS songs_md (
