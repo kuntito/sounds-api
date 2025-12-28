1105cd574f211365bdfeab6e588334beaad07dd0
kuntito <owolabitito@gmail.com>
2025-12-28 22:28:11 +0000

    deleted getSongs end point

--- routes/songRoutes.ts
+++ routes/songRoutes.ts
@@ -1,10 +1,8 @@
 import express from "express";
-import getSongs from "../requestHandler/getSongs";
 import uploadSong from "../requestHandler/uploadSong";
 
 const songRouter = express.Router();
 
-songRouter.get('/songs', getSongs);
 songRouter.post('/song', uploadSong);
 
 export default songRouter;
