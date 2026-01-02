4e60cfab46862415d4fc89be8e3bcf6489e5e5ef
kuntito <owolabitito@gmail.com>
2026-01-02 17:01:09 +0000

    add getNewSongs route

---
+++ requestHandler/getNewSongs.ts
@@ -0,0 +1,82 @@
+import { Request, Response, RequestHandler } from "express";
+import db__sounds_md from "../services/db__soundsMd";
+import getS3ObjectUrl from "../util/getS3ObjectUrl";
+import s3Client from "../services/s3Client";
+
+import { envConfig } from "../config/envConfig";
+
+interface RequestType {
+    songIds: string[];
+}
+
+const getNewSongs: RequestHandler = async (req: Request, res: Response) => {
+    const { songIds: clientSongIds } = req.body as RequestType;
+
+    const clientSongIdsValid = clientSongIds && Array.isArray(clientSongIds);
+    if (!clientSongIdsValid) {
+        return res.status(400).json({
+            success: false,
+            error: "invalid song ids, pass array of strings",
+        });
+    }
+
+
+    const setOfNewIds = getSongIdsClientLacks(clientSongIds);
+    if (setOfNewIds === undefined){
+        return res.status(500).json({
+            success: false,
+            error: "couldn't sync with repo"
+        })
+    }
+
+    const idArray = [...setOfNewIds];
+
+    const urlPromises = idArray.map((id) =>
+        getS3ObjectUrl(s3Client, id, envConfig.AWS_BUCKET_NAME)
+            .catch(err => {
+                console.log(`error occurred: ${(err as Error).message}`);
+            })
+    );
+
+    const newSongUrls = (
+        await Promise.all(urlPromises)
+    ).filter(Boolean);
+
+    return res.status(200).json({
+        success: true,
+        data: newSongUrls,
+    });
+};
+
+export default getNewSongs;
+
+/**
+ * Returns song IDs present in repo but not on client.
+ */
+const getSongIdsClientLacks = (clientIds: string[]): Set<string> | undefined => {
+    const maybeAllSongIds = getAllSongIds();
+    if (maybeAllSongIds === undefined) {
+        return undefined;
+    }
+
+    const idsToReturn = maybeAllSongIds;
+
+    clientIds.forEach((id) => idsToReturn.delete(id));
+
+    return idsToReturn;
+};
+
+
+const getAllSongIds = (): Set<string> | undefined => {
+    try {
+        const ids = db__sounds_md
+            .prepare(`SELECT id FROM songs_md`)
+            .pluck()
+            .all() as string[];
+
+        return new Set(ids);
+    } catch(e) {
+        console.log(`couldn't get all song ids, ${(e as Error).message}`);
+        return undefined;
+    }
+};
--- routes/songRoutes.ts
+++ routes/songRoutes.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import uploadSong from "../requestHandler/uploadSong";
+import getNewSongs from "../requestHandler/getNewSongs";
 
 const songRouter = express.Router();
 
 songRouter.post('/song', uploadSong);
+songRouter.post('/new-songs', getNewSongs);
 
 export default songRouter;
